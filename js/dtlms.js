e0015106beabe56dbcbbfb7887c81e030cbc4d35
hongquan2712 <hongquan2712@gmail.com>
2021-09-09 10:37:32 +0700

    update

--- js/dtlms.js
+++ js/dtlms.js
@@ -3725,13 +3725,23 @@ jQuery.fn.dtLMSToggleItems = function () {
 
     return this.each(function () {
         const elm = this
-        jQuery(elm).toggleClick(function () {
-            jQuery(this).removeClass('active');  
-        }, function() { 
-            jQuery(this).addClass('active'); 
-        });
+        console.log('click dtlms-toggle', { elm });
+
+        // jQuery(elm).toggleClick(function () {
+        //     jQuery(this).removeClass('active');  
+        // }, function() { 
+        //     jQuery(this).addClass('active'); 
+        // });
     
-        jQuery(elm).on('click',function(){ 
+        // jQuery(elm).toggleClick(function (e) {
+        //     console.log('onclick click ', { elm , e});
+        //     jQuery(this).toggleClass('active')
+        // }, function (e) {
+        //     console.log('fn2',{e})
+        // });
+
+        jQuery(elm).on('click', function () {
+            jQuery(this).toggleClass('active');
             jQuery(this).next('.dtlms-toggle-content').slideToggle(); 
         });
     });
